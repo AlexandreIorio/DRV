29540c96bea2026b516e9deffabcafd03bb2dc55
alexandreIorio <alexandre.iorio@heig-vd.ch>
2024-11-09 10:11:50 +0100

    indent

--- material/lab_03/parrot_module/parrot.c
+++ material/lab_03/parrot_module/parrot.c
@@ -1,8 +1,8 @@
-#include <linux/fs.h>      /* Needed for file_operations */
-#include <linux/init.h>    /* Needed for the macros */
-#include <linux/kernel.h>  /* Needed for KERN_INFO */
-#include <linux/module.h>  /* Needed by all modules */
-#include <linux/slab.h>    /* Needed for kmalloc */
+#include <linux/fs.h> /* Needed for file_operations */
+#include <linux/init.h> /* Needed for the macros */
+#include <linux/kernel.h> /* Needed for KERN_INFO */
+#include <linux/module.h> /* Needed by all modules */
+#include <linux/slab.h> /* Needed for kmalloc */
 #include <linux/uaccess.h> /* copy_(to|from)_user */
 
 #include <linux/string.h>
@@ -23,16 +23,17 @@ static int buffer_size;
  * @param swap_lower Swap all lower case letters to upper case.
  * @param swap_upper Swap all upper case letters to lower case.
  */
-static void str_manip(char *str, int swap_lower, int swap_upper) {
-  while (*str != '\0') {
-    if (*str >= 'a' && *str <= 'z' && swap_lower) {
-      *str = *str + ('A' - 'a');
-    } else if (*str >= 'A' && *str <= 'Z' && swap_upper) {
-      *str = *str + ('a' - 'A');
-    }
+static void str_manip(char *str, int swap_lower, int swap_upper)
+{
+	while (*str != '\0') {
+		if (*str >= 'a' && *str <= 'z' && swap_lower) {
+			*str = *str + ('A' - 'a');
+		} else if (*str >= 'A' && *str <= 'Z' && swap_upper) {
+			*str = *str + ('a' - 'A');
+		}
 
-    str++;
-  }
+		str++;
+	}
 }
 
 /**
@@ -46,21 +47,21 @@ static void str_manip(char *str, int swap_lower, int swap_upper) {
  * @return Number of bytes written in the userspace buffer.
  */
 static ssize_t parrot_read(struct file *filp, char __user *buf, size_t count,
-                           loff_t *ppos) {
-  if (buf == 0 || count < buffer_size) {
-    return 0;
-  }
+			   loff_t *ppos)
+{
+	if (buf == 0 || count < buffer_size) {
+		return 0;
+	}
+	// This a simple usage of ppos to avoid infinit loop with `cat`
+	// it may not be the correct way to do.
+	if (*ppos != 0) {
+		return 0;
+	}
+	*ppos = buffer_size;
 
-  // This a simple usage of ppos to avoid infinit loop with `cat`
-  // it may not be the correct way to do.
-  if (*ppos != 0) {
-    return 0;
-  }
-  *ppos = buffer_size;
+	copy_to_user(buf, global_buffer, buffer_size);
 
-  copy_to_user(buf, global_buffer, buffer_size);
-
-  return buffer_size;
+	return buffer_size;
 }
 
 /**
@@ -75,25 +76,26 @@ static ssize_t parrot_read(struct file *filp, char __user *buf, size_t count,
  */
 
 static ssize_t parrot_write(struct file *filp, const char __user *buf,
-                            size_t count, loff_t *ppos) {
-  if (count == 0) {
-    return 0;
-  }
+			    size_t count, loff_t *ppos)
+{
+	if (count == 0) {
+		return 0;
+	}
 
-  *ppos = 0;
+	*ppos = 0;
 
-  if (buffer_size != NULL) {
-    kfree(global_buffer);
-  }
+	if (buffer_size != NULL) {
+		kfree(global_buffer);
+	}
 
-  global_buffer = kmalloc(count + 1, GFP_KERNEL);
+	global_buffer = kmalloc(count + 1, GFP_KERNEL);
 
-  copy_from_user(global_buffer, buf, count);
-  global_buffer[count] = '\0';
+	copy_from_user(global_buffer, buf, count);
+	global_buffer[count] = '\0';
 
-  buffer_size = count + 1;
+	buffer_size = count + 1;
 
-  return count;
+	return count;
 }
 
 /**
@@ -111,64 +113,67 @@ static ssize_t parrot_write(struct file *filp, const char __user *buf,
  */
 
 static long parrot_ioctl(struct file *filep, unsigned int cmd,
-                         unsigned long arg) {
-  if (buffer_size == NULL) {
-    return -1;
-  }
+			 unsigned long arg)
+{
+	if (buffer_size == NULL) {
+		return -1;
+	}
 
-  switch (cmd) {
-  case PARROT_CMD_TOGGLE:
-    str_manip(global_buffer, 1, 1);
-    break;
+	switch (cmd) {
+	case PARROT_CMD_TOGGLE:
+		str_manip(global_buffer, 1, 1);
+		break;
 
-  case PARROT_CMD_ALLCASE:
-    switch (arg) {
-    case TO_UPPERCASE:
-      str_manip(global_buffer, 1, 0);
-      break;
+	case PARROT_CMD_ALLCASE:
+		switch (arg) {
+		case TO_UPPERCASE:
+			str_manip(global_buffer, 1, 0);
+			break;
 
-    case TO_LOWERCASE:
-      str_manip(global_buffer, 0, 1);
-      break;
+		case TO_LOWERCASE:
+			str_manip(global_buffer, 0, 1);
+			break;
 
-    default:
-      return -1;
-    }
-    break;
+		default:
+			return -1;
+		}
+		break;
 
-  default:
-    break;
-  }
-  return 0;
+	default:
+		break;
+	}
+	return 0;
 }
 
 const static struct file_operations parrot_fops = {
-    .owner = THIS_MODULE,
-    .read = parrot_read,
-    .write = parrot_write,
-    .unlocked_ioctl = parrot_ioctl,
+	.owner = THIS_MODULE,
+	.read = parrot_read,
+	.write = parrot_write,
+	.unlocked_ioctl = parrot_ioctl,
 };
 
-static int __init parrot_init(void) {
-  register_chrdev(MAJOR_NUM, DEVICE_NAME, &parrot_fops);
+static int __init parrot_init(void)
+{
+	register_chrdev(MAJOR_NUM, DEVICE_NAME, &parrot_fops);
 
-  buffer_size = 0;
+	buffer_size = 0;
 
-  pr_info("Parrot ready!\n");
-  pr_info("ioctl PARROT_CMD_TOGGLE: %u\n", PARROT_CMD_TOGGLE);
-  pr_info("ioctl PARROT_CMD_ALLCASE: %lu\n", PARROT_CMD_ALLCASE);
+	pr_info("Parrot ready!\n");
+	pr_info("ioctl PARROT_CMD_TOGGLE: %u\n", PARROT_CMD_TOGGLE);
+	pr_info("ioctl PARROT_CMD_ALLCASE: %lu\n", PARROT_CMD_ALLCASE);
 
-  return 0;
+	return 0;
 }
 
-static void __exit parrot_exit(void) {
-  if (global_buffer != NULL) {
-    kfree(global_buffer);
-  }
+static void __exit parrot_exit(void)
+{
+	if (global_buffer != NULL) {
+		kfree(global_buffer);
+	}
 
-  unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
+	unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
 
-  pr_info("Parrot done!\n");
+	pr_info("Parrot done!\n");
 }
 
 MODULE_AUTHOR("REDS");
