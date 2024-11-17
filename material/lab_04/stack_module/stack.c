68bf5756f72f521db1d306827af6976e55a29c9b
alexandreIorio <alexandre.iorio@heig-vd.ch>
2024-11-17 15:38:55 +0100

    work on stack

--- material/lab_04/stack_module/stack.c
+++ material/lab_04/stack_module/stack.c
@@ -11,7 +11,7 @@
 #include <linux/uaccess.h>
 #include <linux/cdev.h>
 #include <linux/device.h>
-
+#include <linux/list.h>
 #include <linux/string.h>
 
 #define MAJOR_NUM 98
@@ -21,11 +21,24 @@
 static struct cdev cdev;
 static struct class *cl;
 
-struct stack {
-	int *data;
-	int size;
-	int capacity;
+static LIST_HEAD(stack);
+
+struct element {
+	int data;
+	struct list_head list;
 };
+
+static int get_stack_size(void)
+{
+	int size = 0;
+	struct element *element;
+
+	list_for_each_entry(element, &stack, list)
+		size++;
+
+	return size;
+}
+
 /**
  * @brief Pop and return latest added element of the stack.
  *
@@ -40,7 +53,47 @@ struct stack {
 static ssize_t stack_read(struct file *filp, char __user *buf, size_t count,
 			  loff_t *ppos)
 {
-	return 0;
+	ssize_t nb_values, stack_size, i;
+	uint32_t *read_values;
+	struct element *element;
+	pr_info("Stack: Reading %d bytes\n", count);
+	if (count % sizeof(uint32_t) != 0) {
+		return -EINVAL;
+	}
+
+	nb_values = count / sizeof(uint32_t);
+	stack_size = get_stack_size();
+
+	if (stack_size == 0) {
+		printk("Stack: Stack is empty\n");
+		return 0;
+	}
+
+	// Check the current stack size and the number of values requested
+	if (nb_values > stack_size) {
+		nb_values = stack_size;
+	}
+
+	read_values = kmalloc(nb_values * sizeof(uint32_t), GFP_KERNEL);
+	if (!read_values)
+		return -ENOMEM;
+
+	for (i = 0; i < nb_values; i++) {
+		element = list_first_entry(&stack, struct element, list);
+		read_values[i] = element->data;
+		list_del(&element->list);
+		kfree(element);
+	}
+
+	if (copy_to_user(buf, read_values, nb_values * sizeof(uint32_t)) != 0) {
+		kfree(read_values);
+		pr_err("Stack: Failed to copy data to user\n");
+		return -EFAULT;
+	}
+	kfree(read_values);
+	// do not return count, because if the stack is smaller than count we
+	// should return the actual number of bytes read
+	return nb_values * sizeof(uint32_t);
 }
 
 /**
@@ -58,11 +111,37 @@ static ssize_t stack_write(struct file *filp, const char __user *buf,
 			   size_t count, loff_t *ppos)
 
 {
-	return 0;
-}
+	ssize_t nb_values;
+	uint32_t *new_values;
+	struct element *elements;
+	ssize_t i;
 
-int stack_open(struct inode *inode, struct file *filp)
-{
+	if (count % sizeof(uint32_t) != 0)
+		return -EINVAL;
+
+	nb_values = count / sizeof(uint32_t);
+	new_values = kmalloc(count, GFP_KERNEL);
+
+	if (!new_values)
+		return -ENOMEM;
+
+	if (copy_from_user(new_values, buf, count) != 0) {
+		kfree(new_values);
+		pr_err("Stack: Failed to copy buffer from user\n");
+		return -EFAULT;
+	}
+	elements = kmalloc_array(nb_values, sizeof(struct element), GFP_KERNEL);
+	if (!elements) {
+		kfree(new_values);
+		pr_err("Stack: Failed to allocate memory for stack elements\n");
+		return -ENOMEM;
+	}
+	for (i = 0; i < nb_values; i++) {
+		elements[i].data = new_values[i];
+		list_add(&elements[i].list, &stack);
+	}
+	kfree(new_values);
+	return count;
 }
 
 /**
@@ -81,7 +160,6 @@ static int stack_uevent(struct device *dev, struct kobj_uevent_env *env)
 static const struct file_operations stack_fops = {
 	.owner = THIS_MODULE,
 	.read = stack_read,
-	.open = stack_open,
 	.write = stack_write,
 };
 
@@ -89,12 +167,7 @@ static int __init stack_init(void)
 {
 	int err;
 
-	struct stack *stack_priv = kmalloc(sizeof(struct stack), GFP_KERNEL);
-
-	if (!stack_priv) {
-		pr_err("Stack: Error allocating memory\n");
-		return -ENOMEM;
-	}
+	printk("\nStack: Initializing\n");
 
 	// Register the device
 	err = register_chrdev_region(MAJMIN, 1, DEVICE_NAME);
@@ -119,8 +192,7 @@ static int __init stack_init(void)
 	}
 
 	cdev_init(&cdev, &stack_fops);
-	// Set the private data of the device
-	*cdev.err = cdev_add(&cdev, MAJMIN, 1);
+	err = cdev_add(&cdev, MAJMIN, 1);
 	if (err < 0) {
 		pr_err("Stack: Adding char device failed\n");
 		device_destroy(cl, MAJMIN);
@@ -135,6 +207,12 @@ static int __init stack_init(void)
 
 static void __exit stack_exit(void)
 {
+	struct element *element, *temp;
+	list_for_each_entry_safe(element, temp, &stack, list) {
+		list_del(&element->list);
+		kfree(element);
+	}
+
 	// Unregister the device
 	cdev_del(&cdev);
 	device_destroy(cl, MAJMIN);
